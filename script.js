3e98fb845f9851aba38ee50ed78b7ae54fcc879a
pjmantoss <joelptoss@gmail.com>
2020-09-26 19:29:40 +0100

    update script

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ rollBtn.addEventListener('click', function(){
         player1Dice.classList.remove('active');
         player2Dice.classList.add('active');
         message.textContent = "Player 2 Turn";
-        console.log(message);
+    } else {
+        Player2Score += randomNumber;
+        player2scoreboard.textContent = Player1Score;
+        player2Dice.textContent = randomNumber;
+        player2Dice.classList.remove('active');
+        player1Dice.classList.add('active');
+        message.textContent = "Player 1 Turn";
     }
 })
