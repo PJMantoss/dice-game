c08795b196e38c1dc60df660f8cd8d3c0a6f7bdc
pjmantoss <joelptoss@gmail.com>
2020-09-26 19:14:25 +0100

    update script

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //Game state variables
 let Player1Score = 0, Player2Score = 0, Player1Turn = true;
 
-//Variables to store refto DOM Nodes
+//Variables to store ref to DOM Nodes
 const player1Dice = document.getElementById('player1dice');
 const player2Dice = document.getElementById('player2dice');
+const player1Scoreboard = document.getElementById('player1scoreBoard');
+const player2Scoreboard = document.getElementById('player2scoreBoard');
